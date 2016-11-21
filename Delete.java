b924b2ef1cf3c90810a61820dcf4e1f9a3705d10
drazhi <drazhi@live.ca>
2016-11-21 17:16:28 -0500

    Add files via upload

--- Delete.java
+++ Delete.java
@@ -3,10 +3,10 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
 
-public class Delete extends JFrame implements ActionListener {
+public class Delet extends JFrame implements ActionListener {
 	JPanel panel;
-	JLabel l1;
-	JTextField t1;
+	JLabel general_employee_l, tickets_l, service_agent_l, assignment_group_l;
+	JCheckBox general_employee_c, tickets_c, service_agent_c, assignment_group_c, del_all_c;
 	String s;
 
 	ResultSet rs;
@@ -18,41 +18,71 @@ public class Delete extends JFrame implements ActionListener {
 
 	JButton b1;
 
-	public Delete() {
+	public Delet() {
 		gbc = new GridBagConstraints();
 		g1 = new GridBagLayout();
 		panel = (JPanel) getContentPane();
 		panel.setLayout(g1);
 
-		l1 = new JLabel("Employ No");
-
-		t1 = new JTextField(10);
-
-		b1 = new JButton("Delete");
-		b1.setMnemonic('D');
-		b1.addActionListener(this);
+		//general_employee_l = new JLabel("General Employee");
+		general_employee_c = new JCheckBox("General Employee");
+		
+		tickets_c = new JCheckBox("Tickets");
+		service_agent_c = new JCheckBox("Service Agent");
+		assignment_group_c = new JCheckBox("Assignment Group");
+		del_all_c = new JCheckBox("Delete All Tables");
+		//t1 = new JTextField(10);
 
 		gbc.anchor = GridBagConstraints.NORTHWEST;
 		gbc.gridx = 1;
-		gbc.gridy = 5;
-		g1.setConstraints(l1, gbc);
-		panel.add(l1);
-
+		gbc.gridy = 1;
+		g1.setConstraints(general_employee_c, gbc);
+		panel.add(general_employee_c);
+		
 		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		g1.setConstraints(tickets_c, gbc);
+		panel.add(tickets_c);
+		
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		g1.setConstraints(service_agent_c, gbc);
+		panel.add(service_agent_c);
+		
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 4;
+		g1.setConstraints(assignment_group_c, gbc);
+		panel.add(assignment_group_c);
+		
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 5;
+		g1.setConstraints(del_all_c, gbc);
+		panel.add(del_all_c);
+		
+		b1 = new JButton("Delete");
+		b1.setMnemonic(KeyEvent.VK_ENTER);
+		b1.addActionListener(this);
+		
+		
+
+	
+
+		/*gbc.anchor = GridBagConstraints.NORTHWEST;
 		gbc.gridx = 4;
 		gbc.gridy = 5;
 		g1.setConstraints(t1, gbc);
-		panel.add(t1);
+		panel.add(t1);*/
 
 		gbc.anchor = GridBagConstraints.NORTHWEST;
 		gbc.gridx = 4;
 		gbc.gridy = 23;
 		g1.setConstraints(b1, gbc);
 		panel.add(b1);
-		
-		this.pack();
-        	this.setLocationRelativeTo(null);
-		
+		this.setLocationRelativeTo(null);
 		try {
 			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 			con = DriverManager.getConnection("jdbc:odbc:java");
@@ -65,9 +95,9 @@ public class Delete extends JFrame implements ActionListener {
 		String str = evt.getActionCommand();
 		if (str.equals("Delete")) {
 			try {
-				int no = Integer.parseInt(t1.getText());
-				ps = con.prepareStatement("delete from javasql where empno=?");
-				ps.setInt(1, no);
+				
+				//JACOB TO GET INFO FROM CHECK BOXES USE 
+				//general_employee_c.isSelected()
 				ps.executeUpdate();
 				JOptionPane.showMessageDialog(this, "Record Deleted");
 			} catch (Exception ex) {
---
+++ Search.java
@@ -0,0 +1,150 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+
+public class Search extends JFrame implements ActionListener {
+	JPanel panel;
+	JLabel l1, l2, l3, l4, l5;
+	JTextField t1, t2, t3, t4, t5;
+	String s;
+
+	ResultSet rs;
+	Connection con;
+	Statement st;
+
+	GridBagLayout g1;
+	GridBagConstraints gbc;
+
+	JButton b1;
+
+	public Search() {
+		gbc = new GridBagConstraints();
+		g1 = new GridBagLayout();
+		panel = (JPanel) getContentPane();
+		panel.setLayout(g1);
+
+		l1 = new JLabel("Employ No");
+		l2 = new JLabel("Name ");
+		l3 = new JLabel("Department");
+		l4 = new JLabel("Designation");
+		l5 = new JLabel("Basic ");
+
+		t1 = new JTextField(10);
+		t2 = new JTextField(10);
+		t3 = new JTextField(10);
+		t4 = new JTextField(10);
+		t5 = new JTextField(10);
+
+		b1 = new JButton("Search");
+		b1.setMnemonic('S');
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 5;
+		g1.setConstraints(l1, gbc);
+		panel.add(l1);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 4;
+		gbc.gridy = 5;
+		g1.setConstraints(t1, gbc);
+		panel.add(t1);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 8;
+		g1.setConstraints(l2, gbc);
+		panel.add(l2);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 4;
+		gbc.gridy = 8;
+		g1.setConstraints(t2, gbc);
+		panel.add(t2);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 11;
+		g1.setConstraints(l3, gbc);
+		panel.add(l3);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 4;
+		gbc.gridy = 11;
+		g1.setConstraints(t3, gbc);
+		panel.add(t3);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 14;
+		g1.setConstraints(l4, gbc);
+		panel.add(l4);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 4;
+		gbc.gridy = 14;
+		g1.setConstraints(t4, gbc);
+		panel.add(t4);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 18;
+		g1.setConstraints(l5, gbc);
+		panel.add(l5);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 4;
+		gbc.gridy = 18;
+		g1.setConstraints(t5, gbc);
+		panel.add(t5);
+
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 4;
+		gbc.gridy = 23;
+		g1.setConstraints(b1, gbc);
+		panel.add(b1);
+
+		this.pack();
+       		this.setLocationRelativeTo(null);
+
+		b1.addActionListener(this);
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			con = DriverManager.getConnection("jdbc:odbc:java");
+			st = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			rs = st.executeQuery("select * from javasql");
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(this, ex);
+		}
+	}
+
+	public void actionPerformed(ActionEvent evt) {
+		String s = evt.getActionCommand();
+		String tt1 = t1.getText();
+		boolean flag = false;
+		int n = Integer.parseInt(tt1);
+		if (s.equals("Search")) {
+			try {
+				rs.first();
+				do {
+					int p = Integer.parseInt(rs.getString("empno"));
+					if (n == p) {
+						t2.setText(rs.getString("nam"));
+						t3.setText(rs.getString("dept"));
+						t4.setText(rs.getString("desig"));
+						t5.setText(rs.getString("basic"));
+						flag = true;
+					}
+
+				} while (rs.next());
+				if (flag == false)
+					JOptionPane.showMessageDialog(this, new String("Record Not Found"));
+			} catch (Exception eee) {
+				JOptionPane.showMessageDialog(this, eee);
+			}
+
+		}
+	}
+
+}
