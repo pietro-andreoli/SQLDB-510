55b77aec2cb3a0ffa4d98413bc102022fa4b0f55
Peter <pandreoli@ryerson.ca>
2016-11-28 20:26:51 -0500

    Got Pia

--- Update.java
+++ Update.java
@@ -32,205 +32,40 @@ public class Update extends JFrame implements ActionListener {
 		panel = (JPanel) getContentPane();
 		panel.setLayout(g1);
 		//If General Employee
-		if(options == 0){
-			this.setSize(500, 500);
-			employee_name_l = new JLabel("Employ No");
-			employee_num_l = new JLabel("Employee Name");
-			employee_name_t = new JTextField(10);
-			employee_num_t = new JTextField(10);
+		
+		this.setSize(500, 500);
+		ticket_num_l = new JLabel("Ticket No");
+		
+		date_closed_l = new JLabel("Date Closed");
 
+		ticket_num_t = new JTextField(10);
+		
+		date_closed_t = new JTextField(10);
 
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		g1.setConstraints(ticket_num_l, gbc);
+		panel.add(ticket_num_l);
 
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 8;
-			g1.setConstraints(employee_num_l, gbc);
-			panel.add(employee_num_l);
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 4;
+		gbc.gridy = 1;
+		g1.setConstraints(ticket_num_t, gbc);
+		panel.add(ticket_num_t);
 
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 5;
-			g1.setConstraints(employee_name_l, gbc);
-			panel.add(employee_name_l);
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		g1.setConstraints(date_closed_l, gbc);
+		panel.add(date_closed_l);
 
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 5;
-			g1.setConstraints(employee_name_t, gbc);
-			panel.add(employee_name_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 8;
-			g1.setConstraints(employee_num_t, gbc);
-			panel.add(employee_num_t);
-
-
-		//If ticket
-		}else if(options == 1){
-			this.setSize(500, 500);
-			ticket_num_l = new JLabel("Ticket No");
-			date_opened_l = new JLabel("Date Opened");
-			date_closed_l = new JLabel("Date Closed");
-			assignment_group_l = new JLabel("Assignment Group");
-			status_l = new JLabel("Status");
-			priority_l = new JLabel("Priority");
-			opened_for_l = new JLabel("Opened For");
-
-			ticket_num_t = new JTextField(10);
-			date_opened_t = new JTextField(10);
-			date_closed_t = new JTextField(10);
-			assignment_group_t = new JTextField(10);
-			status_t = new JTextField(10);
-			priority_t = new JTextField(10);
-			opened_for_t = new JTextField(10);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 1;
-			g1.setConstraints(ticket_num_l, gbc);
-			panel.add(ticket_num_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 1;
-			g1.setConstraints(ticket_num_t, gbc);
-			panel.add(ticket_num_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 2;
-			g1.setConstraints(date_opened_l, gbc);
-			panel.add(date_opened_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 2;
-			g1.setConstraints(date_opened_t, gbc);
-			panel.add(date_opened_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 3;
-			g1.setConstraints(date_closed_l, gbc);
-			panel.add(date_closed_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 3;
-			g1.setConstraints(date_closed_t, gbc);
-			panel.add(date_closed_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 4;
-			g1.setConstraints(assignment_group_l, gbc);
-			panel.add(assignment_group_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 4;
-			g1.setConstraints(assignment_group_t, gbc);
-			panel.add(assignment_group_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 5;
-			g1.setConstraints(status_l, gbc);
-			panel.add(status_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 5;
-			g1.setConstraints(status_t, gbc);
-			panel.add(status_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 6;
-			g1.setConstraints(priority_l, gbc);
-			panel.add(priority_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 6;
-			g1.setConstraints(priority_t, gbc);
-			panel.add(priority_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 7;
-			g1.setConstraints(opened_for_l, gbc);
-			panel.add(opened_for_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 7;
-			g1.setConstraints(opened_for_t, gbc);
-			panel.add(opened_for_t);
-		}else if(options == 2){
-			this.setSize(500, 500);
-			employee_name_l = new JLabel("Employ No");
-			employee_num_l = new JLabel("Employee Name");
-			assignment_group_l = new JLabel ("Assignment Group");
-
-			employee_name_t = new JTextField(10);
-			employee_num_t = new JTextField(10);
-			assignment_group_t = new JTextField(10);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 1;
-			g1.setConstraints(employee_name_l, gbc);
-			panel.add(employee_name_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 1;
-			g1.setConstraints(employee_name_t, gbc);
-			panel.add(employee_name_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 2;
-			g1.setConstraints(employee_num_l, gbc);
-			panel.add(employee_num_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 2;
-			g1.setConstraints(employee_num_t, gbc);
-			panel.add(employee_num_t);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 3;
-			g1.setConstraints(assignment_group_l, gbc);
-			panel.add(assignment_group_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 3;
-			g1.setConstraints(assignment_group_t, gbc);
-			panel.add(assignment_group_t);
-		}else if(option == 3){
-			this.setSize(300, 300);
-			group_name_l = new JLabel ("Group Name");
-
-			group_name_t = new JTextField(10);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 1;
-			gbc.gridy = 3;
-			g1.setConstraints(group_name_l, gbc);
-			panel.add(group_name_l);
-
-			gbc.anchor = GridBagConstraints.NORTHWEST;
-			gbc.gridx = 4;
-			gbc.gridy = 3;
-			g1.setConstraints(group_name_t, gbc);
-			panel.add(group_name_t);
-		}
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridx = 4;
+		gbc.gridy = 3;
+		g1.setConstraints(date_closed_t, gbc);
+		panel.add(date_closed_t);
+		
 
 
 
@@ -258,12 +93,21 @@ public class Update extends JFrame implements ActionListener {
 		if (str.equals("Update")) {
 			try {
 				
-				if(this.option == 0){
+				Connection c = null;
+			    Statement stmt = null;
+			    Class.forName("org.sqlite.JDBC");
+			      c = DriverManager.getConnection("jdbc:sqlite:helpdesk.db");
+			      System.out.println("Opened database successfully");
 
-				}else if(this.option == 1){
-				}else if(this.option == 2){
-				}else if(this.option == 3){
-				}
+			      stmt = c.createStatement();
+
+			    stmt = c.createStatement();
+			    stmt.executeQuery( "UPDATE ticket "
+			    		+ "SET date_closed = '"+date_closed_t.getText()+"', status = 'Closed'"
+			    				+ "WHERE ticket_num = "+ticket_num_t.getText() );
+			      
+			    stmt.close();
+			      c.close();
 
 			} catch (Exception exs) {
 			}
